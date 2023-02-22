10ab2fdf4eb179df5a16422d75b7aa468ca3be1a
HugoSanche <hbmclaren@gmail.com>
2023-02-22 09:41:26 -0600

    Working with casting with classes with Object and var references

--- TestPolimorfismo.java
+++ TestPolimorfismo.java
@@ -6,5 +6,26 @@ public class TestPolimorfismo {
        // theMovie.watchMovie();
        Movie myMovie=Movie.getMovie("Comedy", "Gato con botas");
         myMovie.watchMovie();
+
+        //Le tienes que decir implicitamente que es Adventure
+        //Pero si cambias de "A" por "C" tronara en runtime
+        Adventure movie =(Adventure)Movie.getMovie("A", "Tom Sayer");
+        movie.watchMovie();
+
+        Object comedy=Movie.getMovie("C", "Minios");
+        //comedy.watchMovie(); //Error
+        //Comedy.watchComedy(); //Error
+        Movie movieComedy=(Comedy)comedy;
+        movieComedy.watchMovie();//OK
+        //movieComedy.watchComedy(); //Error watchComedy no esta en Movie
+
+        //Var se introdujo en java 10
+        var moon =Movie.getMovie("S", "Life in the moon");
+        moon.watchMovie();
+        //moon.watchScienceFiction(); Error
+
+        var adventureMovie=new Adventure("Vengadores");
+        adventureMovie.watchAdventure();
+
     }
 }
